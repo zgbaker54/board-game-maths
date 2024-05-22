6c2d22628f5adc732535665750cbafa8d9439d15
Andrew Polk <polkandrew01@gmail.com>
2024-05-22 14:53:43 -0700

    Convert n choose k back to mathjs

--- src/app/shared/helper.ts
+++ src/app/shared/helper.ts
@@ -1,4 +1,5 @@
 import { Card } from './models/deck.model';
+import { combinations } from 'mathjs';
 
 export function CardsCount(card: Card[]): number {
   return card.reduce((prev, card) => prev + (card.count ?? 1), 0);
@@ -8,30 +9,14 @@ export function Unique<T>(array: T[]): T[] {
   return [...new Set<T>(array)];
 }
 
-export function nChooseK(n: number, k: number): number {
-  return nCk(n, k);
-}
-
 /**
  * n choose k
  * @param n
  * @param k
  */
-export function nCk(n: number, k: number): number {
-  if (k < 0) {
-    throw new Error('k must be greater than 0');
-  }
-  if (k > n) {
-    throw new Error('k must be less than or equal to n');
-  }
-
-  return Math.round(factorial(n) / (factorial(k) * factorial(n - k)));
+export function nChooseK(n: number, k: number): number {
+  return nCk(n, k);
 }
-
-export function factorial(n: number): number {
-  let ans = 1;
-
-  if (n === 0) return 1;
-  for (let i = 2; i <= n; i++) ans = ans * i;
-  return ans;
+export function nCk(n: number, k: number): number {
+  return combinations(n, k);
 }
