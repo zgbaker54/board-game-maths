3320f442970593b831edcdaab6a2fd396a54aea7
Andrew Polk <andrew@polklabs.com>
2024-05-22 14:02:21 -0700

    Game object validator

--- src/app/shared/models/game.model.ts
+++ src/app/shared/models/game.model.ts
@@ -15,4 +15,52 @@ export type Expansion = {
   name: string;
 };
 
+export function CheckForErrors(game: Game) {
+  // Check for undefined expansions
+  const expansionIds = new Set<number>();
+  game.decks.forEach((d) => {
+    if (d.expansionId) {
+      expansionIds.add(d.expansionId);
+    } else {
+      // continue
+    }
 
+    d.cards.forEach((c) => {
+      if (c.expansionId) {
+        expansionIds.add(c.expansionId);
+      } else {
+        // continue
+      }
+    });
+  });
+  game.diceSets.forEach((ds) => {
+    if (ds.expansionId) {
+      expansionIds.add(ds.expansionId);
+    } else {
+      // continue
+    }
+
+    ds.dice.forEach((d) => {
+      if (d.expansionId) {
+        expansionIds.add(d.expansionId);
+      } else {
+        // continue
+      }
+    });
+  });
+
+  [...expansionIds].forEach((eId) => {
+    if (game.expansions.find((x) => x.id === eId) === undefined) {
+      console.warn(`Expansion ${eId} is not defined in expansion list`);
+    } else {
+      // continue
+    }
+  });
+
+  // Check for duplicate cards
+  game.decks.forEach(d => {
+    
+  })
+
+  // Check for duplicate deck ids in same expansion
+}
